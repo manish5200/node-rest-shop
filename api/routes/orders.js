f4bb63c7d080f6154e18c93430a95741b2cfb23f
manish5200 <manishksingh5200gmail.com>
2023-07-31 23:34:53 +0530

    Till Managing Orders with Mongoose

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,36 +1,118 @@
 const express = require('express');
 const router = express.Router(); //
+const mongoose = require('mongoose');
 
+const Order = require('../models/order') ;
+const Product = require('../models/product');
 //Handle incoming GET request to /order
 router.get('/' ,(req,res,next) => {
-     res.status(200).json({
-        message: 'Orders were fetched' 
-     });
+        Order.find()
+        .select('product quantity _id')
+        .exec()
+        .then(docs => {
+             res.status(200).json({
+                count : docs.length,
+                orders:docs.map(doc =>{
+                     return {
+                        _id : doc._id,
+                        product: doc.product,
+                        quantity:doc.quantity,
+                        request : {
+                             type:'GET',
+                             url:'http://localhost:3000/orders/' + doc._id
+                        }
+                     }
+                })
+             });
+        })
+        .catch(err => {
+             res.status(500).json({
+                error:err
+             });
+        });
 });
 
 router.post('/' ,(req,res,next) => {
-    const order ={
-        productId : req.body.productId,
-        quantity:req.body.quantity
-    };
-    res.status(201).json({
-       message: 'Orders were Created',
-       orderCreated : order
-    });
+     Product.findById(req.body.productId)
+     .then(product => {
+        if(!product)
+        {
+            return res.status(404).json({
+                message : "Product Not Found"
+            });
+        }
+        const order = new  Order({
+            _id: new mongoose.Types.ObjectId(),
+            quantity: req.body.quantity,
+            product: req.body.productId
+        });
+        return order.save();
+     })
+     .then( result => {
+        console.log(result);
+        res.status(201).json({
+            message : "Order Stored ",
+            createdOrder : {
+                 _id : result._id,
+                 product : result.product,
+                 quantity:result.quantity
+            },
+            request : {
+                type:'GET',
+                url :'http://localhost:3000/orders' + result._id
+            }
+        });
+   })
+   .catch(err =>{
+       console.log(err);  
+       res.status(500).json({
+           error:err
+       });
+   });
 });
 
-router.get('/:ordersId' ,(req,res,next) => {
-    res.status(201).json({
-       message: 'Orders Details' ,
-       ordersId: req.params.ordersId
-    });
+router.get('/:orderId' ,(req,res,next) => {
+     Order.findById(req.params.orderId)
+     .exec()
+     .then(order => {
+        if(!order){
+            return res.status(404).json({
+                message:"No Order Found",
+            });
+        }
+         res.status(200).json({
+            order:order,
+            request:{
+                type : 'GET',
+                url : 'http://localhost:3000/orders'
+            }
+         });
+     })
+     .catch(err =>{
+        res.status(500).json({
+            error:err
+        });
+     });
 });
 
-router.delete('/:ordersId' ,(req,res,next) => {
-    res.status(200).json({
-       message: 'Orders Deleted' ,
-       ordersId: req.params.ordersId
-    });
+router.delete('/:orderId' ,(req,res,next) => {
+      Order.deleteOne({_id: req.params.orderId})
+      .exec()
+      .then(result => {
+        res.status(200).json({
+        message : "Oeder Removed !",
+           request:{
+            type:'POST',
+            url: "http://localhost:3000/orders/",
+            body:{productId:'ID', quantity: 'Number'}
+           }
+        });
+      })
+      .catch(err => {
+          res.status(500).json({
+               error:err
+          });
+      });
 });
 
 module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -40,7 +40,7 @@ router.get("/" , (req, res,next) => {   // Only / not /products beacause  that w
         });
        });
 }); 
-router.post('/' , (req, res,next) => {
+router.post("/" , (req, res,next) => {
     
     const product = new Product({
           _id : new mongoose.Types.ObjectId(),
